bf22c92d743f3dc6c00cd3ba161cd75d116fcb08
ris46ithi <smohamedithiris@gmail.com>
2019-08-27 23:52:40 -0500

    Add files via upload

---
+++ anac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+chmod 777 Anaconda3-2019.07-Linux-x86_64.sh
+./Anaconda3-2019.07-Linux-x86_64.sh
+return 0
